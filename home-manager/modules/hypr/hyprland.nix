5fbaaa8b288a1bad29253299e1c5818df5d8a705
Zeph Levy <171337931+ZephLevy@users.noreply.github.com>
2026-01-07 17:26:56 +0100

    Add flatpak and fix hypr* issues

--- home-manager/modules/hypr/hyprland.nix
+++ home-manager/modules/hypr/hyprland.nix
@@ -178,9 +178,12 @@
     ];
 
 
-    windowrule = [
-      "suppressevent maximize, class:.*"
-      "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
-    ];
+    # windowrule = [
+    #   "suppressevent maximize, class:.*"
+    #   "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+    # ];
+    xwayland = {
+      force_zero_scaling = true;
+    };
   };
 }
--- home-manager/modules/hypr/hyprpaper/hyprpaper.nix
+++ home-manager/modules/hypr/hyprpaper/hyprpaper.nix
@@ -1,12 +1,12 @@
 { config, pkgs, ... }: {
   services.hyprpaper.enable = true;
   services.hyprpaper.settings = {
-    preload = [
-      "/home/zeph/nix/home-manager/modules/hypr/hyprpaper/wallpaper.png"
-    ];
-  
     wallpaper = [
-      ", /home/zeph/nix/home-manager/modules/hypr/hyprpaper/wallpaper.png"
+      {
+        monitor = "";
+        path = "/home/zeph/nix/home-manager/modules/hypr/hyprpaper/wallpaper.png";
+      }
     ];
+    splash = false;
   };
 }
